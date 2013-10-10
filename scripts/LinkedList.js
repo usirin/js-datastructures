2ef131773290c37742234618676118d95b3d1b5f
Umut Sirin <umutsirin1@gmail.com>
2013-10-10 02:37:27 -0400

    Changed the append implementation to work with empty list.

--- scripts/LinkedList.js
+++ scripts/LinkedList.js
@@ -4,12 +4,23 @@ define(['Node'], function(Node) {
   }
 
   LinkedList.prototype.append = function(data) {
+    // checking if the list is empty,
+    // if so, creating a new Node with
+    // the given data, and returning the
+    // list.
+    if(!this.head) {
+      this.head = new Node(data);
+      return this;
+    }
+
+    // this means the list is not empty,
+    // so that we are looping throug to the
+    // last element.
     var tmp = this.head;
     while(tmp.next) {
       tmp = tmp.next;
     }
-    tmp.next = new Node;
-    tmp.next.data = data;
+    tmp.next = new Node(data);
     return this;
   };
 
