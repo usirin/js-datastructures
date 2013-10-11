15496bb4c0f0e41281cd3be344baf6799826c5e0
Umut Sirin <umutsirin1@gmail.com>
2013-10-11 00:30:51 -0400

    append function

--- scripts/DoublyLinkedList.js
+++ scripts/DoublyLinkedList.js
@@ -5,12 +5,52 @@ define(['DoublyNode'], function(Node) {
     this._length = 0;
   }
 
+  DoublyLinkedList.prototype.append = function(data) {
+    if(!data) {
+      console.log('Needs an input.');
+      return false;
+    }
+    
+    if(this._length === 0) {
+      var newNode = new Node(data);
+      this.head = newNode;
+      this.tail = newNode;
+      this._length += 1;
+      return this;
+    }
+
+    var lastNode = this.tail;
+
+    // create a new node.
+    var newNode = new Node(data);
+
+    // attach new node to the last node
+    // as next.
+    lastNode.next = newNode;
+
+    // attach the old list to new node
+    // as previous.
+    newNode.previous = lastNode;
+
+    // attach new node as tail.
+    this.tail = newNode;
+
+    this._length += 1;
+
+    return this;
+  };
+
   /**
    * Prepends an item to the start of the list.
    * @param  {mixed} data
    * @return {DoublyLinkedList}
    */
   DoublyLinkedList.prototype.prepend = function(data) {
+    if(!data) {
+      console.log('Needs an input.');
+      return false;
+    }
+
     if(this._length === 0) {
       var newNode = new Node(data);
       this.head = newNode;
